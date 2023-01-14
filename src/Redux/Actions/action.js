c463d1e74596df06483a6e48d9f6f60a2ea2dbf4
Omrani Mootez Belleh <114734614+Refereeing@users.noreply.github.com>
2023-01-14 03:20:54 +0100

    Add files via upload

--- src/Redux/Actions/action.js
+++ src/Redux/Actions/action.js
@@ -1,1 +1,27 @@
+import { ADD_TASK, CHANGE_DONE, CHANGE_SAVETEXT } from "../Constants/action-types";
 
+export const addTask= (payload) => {
+    return {
+        type:ADD_TASK,
+        payload:payload,
+    }
+
+}
+
+export const changeDone= (payload) => {
+    //console.log("actt", payload);
+    return {
+        type:CHANGE_DONE,
+        payload:payload,
+    };
+
+};
+
+export const changeSaveTask= (payload) => {
+    console.log("actt", payload);
+    return {
+        type:CHANGE_SAVETEXT,
+        payload:payload,
+    };
+
+}
